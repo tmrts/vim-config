fe9e884cfc2600ec672766bb47d9163866dd9c75
TamerTas <tamertas@outlook.com>
2014-02-28 00:21:48 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-vim-config
-==========
+Installation
+------------
+
+To install, run
+
+    git clone git:https://github.com/TamerTas/vim-config.git ~/.vim
+    sh ~/.vim/install.sh
+
+Bundles
+------------
+
+.vimrc.bundles file is used to keep the list of plugins in a clear manner.
+Use it for plugin purposes
--- install.sh
+++ install.sh
@@ -1,31 +1,63 @@
 #!/usr/bin/env bash
 
-[ -z "$VUNDLE_URL" ] && VUNDLE_URL="https://github.com/gmarik/vundle.git"
+config_dir="$HOME/.vim-tt"
 
-msg() {
+[ -z "$VUNDLE_URI" ] && VUNDLE_URI="https://github.com/gmarik/vundle.git"
+[ -z "$CONFIG_URI" ] && CONFIG_URI="https://github.com/TamerTas/vim-config.git"
+
+msg() 
+{
     printf '%b\n' "$1" >&2
 }
 
-error() {
-    msg "\e[31m[✘]\e[0m ${1}${2}"
+success() 
+{
+    if [ "$ret" -eq '0' ]; then
+        msg "${1}${2}"
+    fi
+}
+
+error() 
+{
+    msg "${1}${2}"
     exit 1
 }
 
-program_exists() {
-    local exists='0'
-    type $1 >/dev/null 2>&1 || {local exists='1';}
+program_exists() 
+{
+    local ret='0'
+    type $1 >/dev/null 2>&1 || { local ret='1'; }
 
-    if [ ! "$exists" -eq '0' ]; then
+    # throw error on non-zero return value
+    if [ ! "$ret" -eq '0' ]; then
         error "$2"
     fi
 }
 
-install_vundle() {
-    if [! -d "$HOME/.vim/bundle/vundle"]; then
+link()
+{
+    if [ -e "$1" ]; then
+        ln -sf "$1" "$2"
+    fi
+}
+
+clone_config()
+{
+    if [! -d "$HOME/.vim-tt" ]; then
+        mkdir $HOME/.vim-tt
+    fi
+
+    git clone $CONFIG_URI "$config_dir"
+    success "$1"
+}
+
+install_vundle() 
+{
+    if [! -d "$HOME/.vim/bundle/vundle" ]; then
         mkdir -p $HOME/.vim/bundle/vundle
     fi
 
-    git clone $VUNDLE_URL "$HOME/.vim/bundle/vundle"
+    git clone $VUNDLE_URI "$HOME/.vim/bundle/vundle"
     vim +BundleInstall! +BundleClean +qall
 
     success"$1"
@@ -34,4 +66,11 @@ install_vundle() {
 program_exists "vim" "vim installation is required to continue the installation."
 program_exists "git" "git installation is required to continue the installation."
 
-install_vundle "Successfully installed the bundles"
+clone_config "vim configuration files are cloned successfully."
+
+link "$config_dir/.vimrc"           "$HOME/.vimrc"
+link "$config_dir/.vimrc.bundles"   "$HOME/.vimrc.bundles"
+
+install_vundle "Successfully installed vundle and the bundles"
+
+msg            "Installation is now complete."
